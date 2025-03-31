8a449babe90bfacbcddf73b7e21977bc9a60dae4
Jared Still <jkstill@gmail.com>
2025-03-31 09:46:13 -0700

    corrected the use of adding time to a date - it does NOT use the + sign

--- date/dates.sh
+++ date/dates.sh
@@ -3,6 +3,10 @@
 # examples of using the linux date command
 #
 
+: ${OLD_TZ:='America/Los_Angeles'}
+
+#echo OLD_TZ=$OLD_TZ
+
 #
 banner () {
 	 echo
@@ -41,11 +45,10 @@ banner add time to arbitrary date
 echo date -d "2017-01-01 12:00:00" '+%FT%T %Z'
 date -d "2017-01-01 12:00:00" '+%FT%T %Z'
 echo
-# this works correctly if the formet is set to %FT%T %Z
-# the following commented out line does not work
-#date -d "2017-01-01T12:00:00 +1 hour" '+%FT%T %Z'
-echo date -d "$(date -d '2017-01-01T12:00:00' '+%FT%T %Z') +1 hour" '+%FT%T %Z'
-date -d "$(date -d '2017-01-01T12:00:00' '+%FT%T %Z') +1 hour" '+%FT%T %Z'
+# Add 1 hour to the date
+# Do NOT use th + sign in front of the 1 hour, as then date will interpret is a Time Zone
+echo date -d "2017-01-01T12:00:00 1 hour" '+%FT%T %Z'
+date -d "2017-01-01T12:00:00 1 hour" '+%FT%T %Z'
 echo
 
 
@@ -63,41 +66,39 @@ echo convert epoch time to date - date -d @1483228800
 date -d @1483228800
 echo
 
-echo convert ISO date with TZ to epoch time - date -d "2017-01-01T00:00:00Z" +%s
-date -d "2017-01-01T00:00:00Z" +%s
+echo convert ISO date with TZ to epoch time - date -d 'TZ="America/New_York" 2017-01-01T00:00:00' +%s
+date -d 'TZ="America/New_York" 2017-01-01T00:00:00' +%s
 echo
 
-echo date  -d '2025-03-14 13:36:06.953105+08:00' '+%s'
-date  -d '2025-03-14 13:36:06.953105+08:00' '+%s'
+echo conver the epoch time back to the ISO date with TZ - date -d @1483228800
+echo export TZ='America/New_York'
+echo date  -d 'TZ="America/New_York" @1483246800' '+%Y-%m-%d %H:%M:%S %Z'
+export TZ='America/New_York'
+date  -d 'TZ="America/New_York" @1483246800' '+%Y-%m-%d %H:%M:%S %Z'
 echo
+export TZ=$OLD_TZ
 
 echo convert epoch time to ISO date with TZ - date -d @1483228800 -u
 date -d @1483228800 -u
 echo
 
-echo convert epoch time to ISO date with TZ and T separator - date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%SZ"
-date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%SZ"
+echo convert epoch time to ISO date with TZ and T separator - date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%S%Z"
+echo 'using UTC (-u)'
+date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%S%Z"
 echo
 
-echo add seconds to epoch time - date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%SZ" -d "+1 day"
-echo source date is shown first
-date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%SZ" 
+echo e -d @1483228800 -u +"%Y-%m-%dT%H:%M:%SZ" 
+date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%S%Z" 
 echo
 
-# it would be nice if this worked: date -d '@1483228800 + 1 day' -u +"%Y-%m-%dT%H:%M:%SZ" 
-# but it does not
-# this does work:  date -d "$(date -d '@000.00234' '+%FT%T.%N %Z') + 1 hour"
-# the epoch must be converted to a date string first
 
-echo add 1 hour to epoch time - date -d @1483228800 -u +"%Y-%m-%dT%H:%M:%SZ" -d "+1 hour"
-date -d "$(date -d '@1483228800.00234' '+%FT%T.%N %Z')" '+%FT%T %Z'
-date -d "$(date -d '@1483228800.00234' '+%FT%T.%N %Z') +1 hour" '+%FT%T %Z'
+echo add 1 hour and 1 day  to epoch time 
+date -d "$(date -d '@1483228800.00234' )" '+%FT%T %Z'
+date -d "$(date -d '@1483228800.00234' ) 1 hour" '+%FT%T %Z'
+date -d "$(date -d '@1483228800.00234' ) 1 day" '+%FT%T %Z'
 echo
 
-echo subtrace 76 seconds from an epoch time - date -d "$(date -d '@1483228800' '+%FT%T %Z') -76 seconds" '+%FT%T %Z'
+echo subtract 76 seconds from an epoch time - date -d "$(date -d '@1483228800' '+%FT%T %Z') -76 seconds" '+%FT%T %Z'
+date -d "$(date -d '@1483228800' '+%FT%T %Z')" '+%FT%T %Z'
 date -d "$(date -d '@1483228800' '+%FT%T %Z') -76 seconds" '+%FT%T %Z'
-
-
-
-
 
