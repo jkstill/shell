0ff13a8a53b41a96fa40a22efa0077e53ab9de21
Jared Still <jkstill@gmail.com>
2026-01-04 10:25:57 -0800

    interim changes to bash debugging

--- array-ops/extend-array.sh
+++ array-ops/extend-array.sh
@@ -4,14 +4,28 @@ set -u
 
 declare -a ary=()
 
+#check if array is empty
+[[ ${#ary[@]} -eq 0 ]] && echo "Array is empty"
+
 ary+=(one)
 ary+=(two)
 ary+=(three)
+
+[[ ${#ary[@]} -gt 0 ]] && echo "Array is not empty"
 
 i=0
 while [[ $i -lt ${#ary[@]} ]]
 do
 	echo "$i: ${ary[$i]}"
 	(( i++ ))
+done
+
+echo
+echo "Using for loop to iterate over array indices"
+echo 
+
+for i in ${!ary[@]}
+do
+	echo "$i: ${ary[$i]}"
 done
 
--- debug-single-step/debug-single-step.sh
+++ debug-single-step/debug-single-step.sh
@@ -1,19 +1,54 @@
 #!/usr/bin/env bash
 
-trap 'echo "# main() - $BASH_COMMAND";read' DEBUG
+set -auo pipefail
+
+<< COMMENT
+
+This script demonstrates single-stepping through functions using the DEBUG trap.
+
+When you run this script, it will pause before executing each command, displaying the line numbers, function name, and the command about to be executed.
+
+BASH_LINENO: Line where the current function was called.
+LINENO: Current line number in the script.
+
+COMMENT
+
+trap 'echo "# $LINENO:$BASH_LINENO main() - $BASH_COMMAND";read' DEBUG
 
 f1 () {
-	trap 'echo "# f1() - $BASH_COMMAND";read' DEBUG
+	trap 'echo "# $LINENO:$BASH_LINENO f1() - $BASH_COMMAND";read' DEBUG
 	date
+	hostname
 }
 
 f2 () {
-	trap 'echo "# f2() - $BASH_COMMAND";read' DEBUG
+	trap 'echo "# $LINENO:$BASH_LINENO f2() - $BASH_COMMAND";read' DEBUG
 	date
+	hostname
 }
-
 
 date
 f1
 f2
 hostname
+
+echo "traps: $(trap -p)"
+trap DEBUG # clear trap on DEBUG signal
+echo "traps after clear: $(trap -p)"
+
+echo
+echo test
+echo
+
+# this could be dangerous, use with discretion
+# show the result of the executed command 
+# ++1 will be executed in a subshell for the trap, not affecting the main shell
+i=0
+trap 'echo "# $LINENO:$BASH_LINENO main() - $BASH_COMMAND";echo  $(eval "echo eval:; $BASH_COMMAND");' DEBUG
+
+date
+(( ++i ))
+echo "i: $i"
+
+trap DEBUG
+
