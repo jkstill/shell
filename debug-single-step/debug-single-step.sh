4c25cd3a24c65f914dbb10c50378b5693e2f7db5
Jared Still <jkstill@gmail.com>
2026-01-04 10:49:52 -0800

    update for use in a blog

--- debug-single-step/debug-single-step.sh
+++ debug-single-step/debug-single-step.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+declare DEBUG=${DEBUG:-'N'}
+
 set -auo pipefail
 
 << COMMENT
@@ -8,47 +10,58 @@ This script demonstrates single-stepping through functions using the DEBUG trap.
 
 When you run this script, it will pause before executing each command, displaying the line numbers, function name, and the command about to be executed.
 
-BASH_LINENO: Line where the current function was called.
 LINENO: Current line number in the script.
+BASH_LINENO: Line where the current function was called.
 
 COMMENT
 
-trap 'echo "# $LINENO:$BASH_LINENO main() - $BASH_COMMAND";read' DEBUG
+debugMsg () {
+	[[ "$DEBUG" == 'Y' ]] && {
+		#echo
+		echo "DEBUG: $*"
+		#echo
+	}
+}
+
+[[ $DEBUG == 'Y' ]] && trap 'echo "# $LINENO:$BASH_LINENO main() - $BASH_COMMAND";read' DEBUG
 
 f1 () {
-	trap 'echo "# $LINENO:$BASH_LINENO f1() - $BASH_COMMAND";read' DEBUG
+	[[ $DEBUG == 'Y' ]] && trap 'echo "# $LINENO:$BASH_LINENO f1() - $BASH_COMMAND";read' DEBUG
+	debugMsg "In f1()"
+	debugMsg "Calling date"
 	date
-	hostname
 }
 
-f2 () {
-	trap 'echo "# $LINENO:$BASH_LINENO f2() - $BASH_COMMAND";read' DEBUG
-	date
-	hostname
+# single step eval commands
+ssEval () {
+	local lineno=$1
+	local bash_lineno=$2
+	shift 2
+	echo "# $lineno:$bash_lineno $(caller) - $*"	
+	read
+	echo "eval:  $(eval "$*")"
 }
 
+
+debugMsg "In main()"
+debugMsg "Calling date, f1"
 date
 f1
-f2
-hostname
 
 echo "traps: $(trap -p)"
 trap DEBUG # clear trap on DEBUG signal
 echo "traps after clear: $(trap -p)"
 
-echo
-echo test
-echo
-
-# this could be dangerous, use with discretion
+# the following could be dangerous, use with discretion
 # show the result of the executed command 
 # ++1 will be executed in a subshell for the trap, not affecting the main shell
 i=0
-trap 'echo "# $LINENO:$BASH_LINENO main() - $BASH_COMMAND";echo  $(eval "echo eval:; $BASH_COMMAND");' DEBUG
+[[ $DEBUG == 'Y' ]] && trap 'ssEval $LINENO $BASH_LINENO "$BASH_COMMAND";' DEBUG
 
 date
 (( ++i ))
 echo "i: $i"
 
-trap DEBUG
+[[ $DEBUG == 'Y' ]] && trap DEBUG
+
 
